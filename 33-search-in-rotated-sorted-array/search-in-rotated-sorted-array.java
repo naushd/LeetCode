bc05ce068e01784e8e16180650c1caef18f8c013
naushd <91519997+naushd@users.noreply.github.com>
2026-06-10 00:33:37 -0500

    Time: 0 ms (100.00%) | Memory: 44 MB (26.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,38 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        return binarySearch(nums, n, target);
-    }
+       int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right){
 
-    static int binarySearch(int[] nums, int n, int target){
-        int low = 0, high = n-1;
-
-        while(low <= high){
-            int mid = low + (high-low)/2;
+            mid = left + (right-left)/2;
 
             if(nums[mid] == target){
                 return mid;
             }
 
-            //left sorted: 
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid-1;
-                } else {
-                    low = mid+1;
+            if(nums[left] <= nums[mid]){
+                if(target >= nums[left] && target <= nums[mid]){
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
                 }
-            } 
-            
-            //right sorted
-            else {
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid+1;
-                } else {
-                    high = mid-1;
+            }else {
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }else {
+                    right = mid - 1;                     
                 }
+                
             }
         }
-
         return -1;
     }
 }
